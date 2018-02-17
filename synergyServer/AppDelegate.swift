90ca6c1ea80a7e874f686c26e558c4e8ccc1eee0
chegde <chegde@foxmail.com>
2018-02-17 15:15:08 +0800

    Initial Commit

---
+++ synergyServer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  synergyServer
+//
+//  Created by liuchengde on 2018/2/17.
+//  Copyright © 2018年 liuchengde. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ synergyServer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  synergyServer
+//
+//  Created by liuchengde on 2018/2/17.
+//  Copyright © 2018年 liuchengde. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
